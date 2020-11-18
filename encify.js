39054cb781a7fa6abc3f59cf58da276faf9c98d0
Akın Özgen <akinozgen17@outlook.com>
2020-11-18 14:50:20 +0200

    Add files via upload

---
+++ encify.js
@@ -0,0 +1,39 @@
+const encify = function(enc = 'base64', type = 'json') {
+    const encoders = {
+        base64: {
+            json: {
+                encode(o) {
+                    return btoa(unescape(encodeURIComponent(JSON.stringify(o))));
+                },
+
+                decode(str) {
+                    return JSON.parse(atob(decodeURIComponent(escape(str))));
+                }
+            },
+            string: {
+                encode(str) {
+                    return btoa(unescape(encodeURIComponent((str))));
+                },
+                decode(str) {
+                    return (atob(escape(decodeURIComponent(str))));
+                }
+            }
+        },
+        httpQuery: {
+            json: {
+                serialize(o) {
+                    return Object.keys(o).map(key => `${key}=${o[key]}`).join('&')
+                },
+                deserialize(str) {
+                    const o = {};
+                    str.split('&').forEach(i => {
+                        const ps = i.split('=');
+                        o[ps[0]] = ps[1];
+                    });
+                    return o;
+                }
+            }
+        }
+    };
+    return encoders[enc][type];
+};
